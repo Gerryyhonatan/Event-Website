685a4d41b6dcc5bc4813ac89e20a9a60642741cc
gerry <gerryyhonatan29@gmail.com>
2024-12-12 18:14:34 +0700

    Fourth Push

--- app/api/v1/images/controller.js
+++ app/api/v1/images/controller.js
@@ -3,8 +3,10 @@ const { createImages } = require('../../../services/mongoose/images');
 
 const { StatusCodes } = require('http-status-codes');
 
-const create = async (req, res) => {
+const create = async (req, res, next) => {
   try {
+    console.log('req.file')
+    console.log(req.file)
     const result = await createImages(req);
 
     res.status(StatusCodes.CREATED).json({
