2559976d45c7d83e61279959879ef6f6160ac04c
gerry <gerryyhonatan29@gmail.com>
2024-12-14 13:55:12 +0700

    Six Push

--- app/errors/index.js
+++ app/errors/index.js
@@ -1,9 +1,13 @@
 const CustomAPIError = require('./custom-api-error');
 const BadRequestError = require('./bad-request');
 const NotFoundError = require('./not-found');
+const UnautorizhedError = require('./unautorizhed');
+const UnauthenticatedError = require('./unauthenticated');
 
 module.exports = {
   CustomAPIError,
   BadRequestError,
   NotFoundError,
+  UnautorizhedError,
+  UnauthenticatedError
 };
